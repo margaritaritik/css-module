cc27be55e3941a4fd34797f52388329db1df998e
margaritaritik <margaritakuryaeva@gmail.com>
2022-07-14 21:32:52 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const container = {
+    padding: 12,
+    fill:'red'
+    // background: 'red'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ellipse key="ellipce1" style={container}>
+
+      </ellipse>
     </div>
   );
 }
