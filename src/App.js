3560c7bceb35c390d9921b1324f2690367b46873
margaritaritik <margaritakuryaeva@gmail.com>
2022-07-15 22:20:45 +0700

    test animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from "react";
+import {Spring, animated, useSpring} from 'react-spring'
 
 function App() {
   const container = {
-    padding: 12,
-    fill:'red'
-    // background: 'red'
-  }
-  return (
-    <div className="App">
-      <ellipse key="ellipce1" style={container}>
+      width:'30px',
+      height:'30px',
+      background:'cyan',
 
-      </ellipse>
-    </div>
+      borderRadius:'50%',
+        // position:'absolute',
+        top:'0',
+        // bottom:'0',
+        // left:'0',
+        // right:'0',
+        overflow:'hidden',
+    // margin:auto;
+    // animation: circle 6s linear infinite;
+  }
+  const divStyle = {
+      display: 'flex',
+      justifyContent: 'center'
+  }
+    const props = useSpring({
+        opacity: 0,
+        from: { opacity: 3 },
+    });
+
+  return (
+      <animated.div style={props}>
+          Помогите! Я исчезаю
+      </animated.div>
+    // <div className="App" style={props} >
+    //   <div className="cercle" style={container}></div>
+    // </div>
   );
 }
 
