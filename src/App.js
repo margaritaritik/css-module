2eafa799bf8adb4de5cbad3e25d5151df5034e2d
margaritaritik <margaritakuryaeva@gmail.com>
2022-09-09 20:53:07 +0700

    create new animation block

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import {Spring, animated, useSpring} from 'react-spring'
 import ChangeColorText from "./components/ChangeColorText";
 import Cloud_animation from "./components/animation elements/Cloud_animation";
+import BlockDestruction from "./components/block destruction/BlockDestruction";
 
 function App() {
     return (
         <div>
-            {/*<ChangeColorText/>*/}
-            <Cloud_animation/>
+           <BlockDestruction/>
         </div>
     );
 
---
+++ src/components/block destruction/BlockDestruction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './block_destruction_styles.module.css'
+
+const BlockDestruction = () => {
+    return (
+        <div className={styles.container}>
+            <div>TEXT</div>
+        </div>
+    );
+};
+
+export default BlockDestruction;
