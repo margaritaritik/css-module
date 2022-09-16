d2cecef2296c05d4c7c31e764a2a16b83e9d7f84
margaritaritik <margaritakuryaeva@gmail.com>
2022-09-16 21:47:04 +0700

    new animation Time Of Day

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import ChangeColorText from "./components/ChangeColorText";
 import Cloud_animation from "./components/animation elements/Cloud_animation";
 import BlockDestruction from "./components/block destruction/BlockDestruction";
 import Revert from "./components/Revert";
+import TimeOfDay from "./components/animation time of day/TimeOfDay";
 
 function App() {
     return (
         <div>
-          <Revert s='привет!'/>
+          <TimeOfDay/>
         </div>
     );
 
---
+++ src/components/animation time of day/TimeOfDay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './style_timeOfDay.module.css'
+
+const TimeOfDay = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.sunrise}>
+
+            </div>
+        </div>
+    );
+};
+
+export default TimeOfDay;
