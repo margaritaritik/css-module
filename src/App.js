d029e34540259827161218aafc85d66966b2f4de
margaritaritik <margaritakuryaeva@gmail.com>
2022-10-24 23:11:07 +0700

    moon

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Cloud_animation from "./components/animation elements/Cloud_animation";
 import BlockDestruction from "./components/block destruction/BlockDestruction";
 import Revert from "./components/Revert";
 import TimeOfDay from "./components/animation time of day/TimeOfDay";
+import Moon from "./components/animation time of day/Moon";
 
 function App() {
     return (
         <div>
-         <TimeOfDay/>
+         <Moon/>
         </div>
     );
 
