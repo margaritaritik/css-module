5f2eaa0c44ae59b40350a40e2d032a218e602fda
margaritaritik <margaritakuryaeva@gmail.com>
2022-10-28 13:55:02 +0700

    starfall

--- src/components/animation time of day/Moon.jsx
+++ src/components/animation time of day/Moon.jsx
@@ -4,6 +4,16 @@ import styles from './moon_style.module.css';
 const Moon = () => {
     return (
         <div className={styles.container}>
+            <div className={styles.stars}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <div className={styles.container_moon}>
                 <li className={styles.spot}></li>
                 <li className={styles.spot}></li>
